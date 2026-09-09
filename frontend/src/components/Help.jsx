f2037ad6542582157313ad5b5f5dc6acbf860fd3
Even Nordstad <even.nordstad@scania.com>
2026-09-09 13:28:19 +0200

    update date

--- frontend/src/components/Help.jsx
+++ frontend/src/components/Help.jsx
@@ -1036,7 +1036,7 @@ const LegalSection = () => (
     
     <h2 className="text-2xl font-semibold text-[var(--ink-2)] mb-4">MIT License</h2>
     <div className="bg-[var(--ground-2)] border border-[var(--rule-hair)] rounded-lg p-4 font-mono text-sm">
-      <p>Copyright (c) 2024 Planqer</p>
+      <p>Copyright (c) 2026 Planqer</p>
       <br />
       <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the"Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
       <br />
